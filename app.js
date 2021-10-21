cb2f8576e9d895a7ae7d39c9c405c48e6350dd95
CrappyDill <mddillon47@gmail.com>
2021-10-20 17:56:07 -0700

    basic express server setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const ExpressError = require("./expressError");
+const morgan = require("morgan");
+const app = express();
+
+//request body to json
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
+
+app.use(function (req, res, next) {
+  const error = new ExpressError("Not Found", 404);
+
+  return next(error);
+});
+
+app.use(function (error, req, res, next) {
+  res.status(error.status || 500);
+
+  return res.json({
+    error: error,
+    message: error.message,
+  });
+});
+
+module.exports = app;
