8c212e6baeb35531156b914e76c5a00a258c1a87
CrappyDill <mddillon47@yahoo.com>
2021-11-03 10:38:31 -0700

    companie routes done

--- routes/companies.js
+++ routes/companies.js
@@ -5,7 +5,9 @@ const db = require("../db");
 
 router.get("/", async function (req, res, next) {
   try {
-    const results = await db.query(`SELECT code, name, description FROM companies`);
+    const results = await db.query(
+      `SELECT code, name, description FROM companies`
+    );
     return res.json({ companies: results.rows });
   } catch (err) {
     return next(err);
@@ -36,6 +38,31 @@ router.get("/:code", async function (req, res, next) {
       throw new ExpressError("Code not found", 404);
     }
     return res.json({ companies: results.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.patch("/:code", async function (req, res, next) {
+  try {
+    const { code } = req.params;
+    const { name, description } = req.body;
+    const results = await db.query(
+      `UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`,
+      [name, description, code]
+    );
+    return res.send(results.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete("/:code", async function (req, res, next) {
+  try {
+    const results = await db.query(`DELETE FROM companies WHERE code=$1`, [
+      req.params.code,
+    ]);
+    return res.json({ status: "Deleted" });
   } catch (err) {
     return next(err);
   }
