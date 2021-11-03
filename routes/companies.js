3f2a1863bf8eb52c0feb50e5bde97c0f0f0f3fc2
Matthew Dillon <mddillon47@gmail.com>
2021-11-02 18:27:10 -0700

    companies route

--- routes/companies.js
+++ routes/companies.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const ExpressError = require("../expressError");
+const router = express.Router();
+const db = require("../db");
+
+router.get("/", async (err, req, res, next) => {
+  try {
+    const { code } = req.params;
+    const companies = await db.query(`SELECT * FROM companies`);
+    if (companies.length === 0) {
+      throw new ExpressError(`Can't find company with code ${code}`, 404);
+    }
+    return res.json({ companies: companies.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
