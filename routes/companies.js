1b1b1130ed04d9702ff69f9c7edf61eb282b45a7
CrappyDill <mddillon47@yahoo.com>
2021-11-03 08:48:46 -0700

    :code error handling

--- routes/companies.js
+++ routes/companies.js
@@ -5,9 +5,23 @@ const db = require("../db");
 
 router.get("/", async function (req, res, next) {
   try {
+    const result = await db.query(`SELECT code, name FROM companies`);
+    return res.json({ companies: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/:code", async function (req, res, next) {
+  try {
+    const { code } = req.params;
     const result = await db.query(
-      `SELECT code, name FROM companies`
+      `SELECT code, name, description FROM companies WHERE companies.code = $1`,
+      [code]
     );
+    if (result.rowCount === 0) {
+      throw new ExpressError("Code not found", 404);
+    }
     return res.json({ companies: result.rows });
   } catch (err) {
     return next(err);
