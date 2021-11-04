d55af39ba285177384a2aa0d0d6815ef7e62c23a
CrappyDill <mddillon47@yahoo.com>
2021-11-03 19:34:23 -0700

    patch invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -12,18 +12,18 @@ router.get("/", async function (req, res, next) {
   }
 });
 
-// router.post("/", async function (req, res, next) {
-//   try {
-//     const { code, name, description } = req.body;
-//     const results = await db.query(
-//       `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`,
-//       [code, name, description]
-//     );
-//     return res.status(201).json(results.rows[0]); //Add Status 201 for created
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+router.post("/", async function (req, res, next) {
+  try {
+    const { comp_code, amt } = req.body;
+    const results = await db.query(
+      `INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *`,
+      [id, comp_code, amt, paid, add_date, paid_date]
+    );
+    return res.status(201).json(results.rows[0]); //Add Status 201 for created
+  } catch (err) {
+    return next(err);
+  }
+});
 
 router.get("/:id", async function (req, res, next) {
   try {
@@ -35,8 +35,15 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
-router.gpost("/", async function (req, res, next) {
+router.patch("/:id", async function (req, res, next) {
   try {
+    const { id } = req.params;
+    const { amt } = req.body;
+    const results = await db.query(
+      `UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`,
+      [amt, id]
+    );
+    return res.send(results.rows[0]);
   } catch (err) {
     return next(err);
   }
