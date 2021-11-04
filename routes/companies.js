fd8ebea2bf5e40008d341494a7395b7b8fcd1bf8
CrappyDill <mddillon47@yahoo.com>
2021-11-03 19:44:53 -0700

    done

--- routes/companies.js
+++ routes/companies.js
@@ -29,15 +29,32 @@ router.post("/", async function (req, res, next) {
 
 router.get("/:code", async function (req, res, next) {
   try {
-    const { code } = req.params;
-    const results = await db.query(
-      `SELECT code, name, description FROM companies WHERE companies.code = $1`,
+    let code = req.params.code;
+
+    const compResult = await db.query(
+      `SELECT code, name, description
+           FROM companies
+           WHERE code = $1`,
       [code]
     );
-    if (results.rowCount === 0) {
-      throw new ExpressError("Code not found", 404);
+
+    const invResult = await db.query(
+      `SELECT id
+           FROM invoices
+           WHERE comp_code = $1`,
+      [code]
+    );
+
+    if (compResult.rows.length === 0) {
+      throw new ExpressError(`No such company: ${code}`, 404);
     }
-    return res.json({ companies: results.rows });
+
+    const company = compResult.rows[0];
+    const invoices = invResult.rows;
+
+    company.invoices = invoices.map((inv) => inv.id);
+
+    return res.json({ company: company });
   } catch (err) {
     return next(err);
   }
--- routes/invoices.js
+++ routes/invoices.js
@@ -19,6 +19,9 @@ router.post("/", async function (req, res, next) {
       `INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *`,
       [id, comp_code, amt, paid, add_date, paid_date]
     );
+    if (results.rowCount === 0) {
+      throw new ExpressError("ID not found", 404);
+    }
     return res.status(201).json(results.rows[0]); //Add Status 201 for created
   } catch (err) {
     return next(err);
@@ -29,6 +32,9 @@ router.get("/:id", async function (req, res, next) {
   try {
     const { id } = req.params;
     const result = await db.query(`SELECT * FROM invoices WHERE id = $1`, [id]);
+    if (result.rowCount === 0) {
+      throw new ExpressError("ID not found", 404);
+    }
     return res.json({ invoices: result.rows });
   } catch (err) {
     return next(err);
@@ -43,21 +49,21 @@ router.patch("/:id", async function (req, res, next) {
       `UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`,
       [amt, id]
     );
+    if (results.rowCount === 0) {
+      throw new ExpressError("ID not found", 404);
+    }
     return res.send(results.rows[0]);
   } catch (err) {
     return next(err);
   }
 });
 
-router.get("/", async function (req, res, next) {
+router.delete("/:id", async function (req, res, next) {
   try {
-  } catch (err) {
-    return next(err);
-  }
-});
-
-router.get("/", async function (req, res, next) {
-  try {
+    const results = await db.query(`DELETE FROM invoices WHERE id=$1`, [
+      req.params.id,
+    ]);
+    return res.json({ status: "Deleted" });
   } catch (err) {
     return next(err);
   }
