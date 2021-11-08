23e09e032985f5006cb57d7516564a4be0f4d3d9
CrappyDill <mddillon47@yahoo.com>
2021-11-08 09:04:09 -0800

    pay invoice and industies setup

--- routes/invoices.js
+++ routes/invoices.js
@@ -43,16 +43,40 @@ router.get("/:id", async function (req, res, next) {
 
 router.patch("/:id", async function (req, res, next) {
   try {
-    const { id } = req.params;
-    const { amt } = req.body;
-    const results = await db.query(
-      `UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`,
-      [amt, id]
+    let { amt, paid } = req.body;
+    let id = req.params.id;
+    let paidDate = null;
+
+    const currResult = await db.query(
+      `SELECT paid
+           FROM invoices
+           WHERE id = $1`,
+      [id]
     );
-    if (results.rowCount === 0) {
-      throw new ExpressError("ID not found", 404);
+
+    if (currResult.rows.length === 0) {
+      throw new ExpressError(`No invoice: ${id}`, 404);
     }
-    return res.send(results.rows[0]);
+
+    const currPaidDate = currResult.rows[0].paid_date;
+
+    if (!currPaidDate && paid) {
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null;
+    } else {
+      paidDate = currPaidDate;
+    }
+
+    const result = await db.query(
+      `UPDATE invoices
+           SET amt=$1, paid=$2, paid_date=$3
+           WHERE id=$4
+           RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [amt, paid, paidDate, id]
+    );
+
+    return res.json({ invoice: result.rows[0] });
   } catch (err) {
     return next(err);
   }
