5812c7f950b06570cdb71cfaa442202e812c6c1b
CrappyDill <mddillon47@yahoo.com>
2021-11-08 19:08:55 -0800

    industries

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use("/companies", companyRoute);
 const invoiceRoute = require("./routes/invoices");
 app.use("/invoices", invoiceRoute);
 
+const industryRoute = require("./routes/industries");
+app.use("/industries", industryRoute);
+
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.use(function (req, res, next) {
